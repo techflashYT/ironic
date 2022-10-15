techflashYT/ironic

commit 38cf46410bb9e6cfa62cc5346e100703b9e1e188

read_string no longer panics on error

core/src/dbg/ios.rs
@@ -64,8 +64,8 @@ macro_rules! scdef {
 /// Read a NUL-terminated string from memory.  
 /// 
 /// NOTE: This is not particularly rigorous or safe.
-pub fn read_string(cpu: &Cpu, ptr: u32) -> String {
-    let paddr = cpu.translate(TLBReq::new(ptr, Access::Debug)).expect("FIXME");
+pub fn read_string(cpu: &Cpu, ptr: u32) -> Result<String, String> {
+    let paddr = cpu.translate(TLBReq::new(ptr, Access::Debug))?;
 
     let mut line_buf = [0u8; 64];
     cpu.bus.read().unwrap().dma_read(paddr, &mut line_buf);
@@ -80,7 +80,7 @@ pub fn read_string(cpu: &Cpu, ptr: u32) -> String {
     } else {
         std::str::from_utf8(&line_buf).unwrap()
     };
-    s.trim_matches(char::from(0)).to_string()
+    Ok(s.trim_matches(char::from(0)).to_string())
 }
 
 pub fn get_syscall_desc(idx: u32) -> Option<SyscallDef> {

commit acd40f36fdedd0e0c6fe7357ef08fb566f4e1ea7

Forgot to fix users of read_string from recent changes
Fixes: 38cf464

core/src/cpu/excep.rs
@@ -85,7 +85,9 @@ impl Cpu {
 
 
         match e {
-            ExceptionType::Undef(opcd) => ios::resolve_syscall(self, opcd),
+            ExceptionType::Undef(opcd) => {
+                ios::resolve_syscall(self, opcd).unwrap_or_default(); // Just a println. Ok to fail
+            },
             _ => {},
         }
 

core/src/dbg/ios.rs
@@ -165,12 +165,12 @@ pub fn get_syscall_desc(idx: u32) -> Option<SyscallDef> {
 
 
 /// Resolve information about an IOS syscall and its arguments.
-pub fn resolve_syscall(cpu: &mut Cpu, opcd: u32) {
+pub fn resolve_syscall(cpu: &mut Cpu, opcd: u32) -> Result<(), String> {
     // Get the syscall index (and ignore some)
     let idx = (opcd & 0x00ff_ffe0) >> 5;
     let res = get_syscall_desc(idx);
     if res.is_none() {
-        return;
+        return Ok(());
     }
     let syscall = res.unwrap();
     let mut arg_buf = String::new();
@@ -181,7 +181,10 @@ pub fn resolve_syscall(cpu: &mut Cpu, opcd: u32) {
                 arg_buf.push_str(&format!("0x{:08x}", val));
             },
             ArgType::StrPtr => {
-                let s = read_string(cpu, val);
+                let s = match read_string(cpu, val){
+                    Ok(val) => val,
+                    Err(reason) => {return Err(reason);},
+                };
                 arg_buf.push_str(&format!("\"{}\"", s));
             },
             ArgType::Int => {
@@ -199,5 +202,6 @@ pub fn resolve_syscall(cpu: &mut Cpu, opcd: u32) {
         ExecutionCtx::from(cpu.read_fetch_pc()),
         syscall.name, arg_buf, cpu.reg[Reg::Lr]
     );
+    Ok(())
 }
 