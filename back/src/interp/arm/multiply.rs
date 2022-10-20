4068bda955b65f695dba3f8ec8909841da4740a9
Andrew Piroli <drewpiroli@gmail.com>
2022-10-20 13:36:02 -0400

    Backend: Interp: fix unusual byte groupings

--- back/src/interp/arm/multiply.rs
+++ back/src/interp/arm/multiply.rs
@@ -8,8 +8,8 @@ pub fn umull(cpu: &mut Cpu, op: SignedMlBits) -> DispatchRes {
     let rn_val = cpu.reg[op.rn()] as usize;
     let res = rm_val * rn_val;
 
-    let res_hi = ((res & 0xffffffff_00000000) >> 32) as u32;
-    let res_lo =  (res & 0x00000000_ffffffff) as u32;
+    let res_hi = ((res & 0xffff_ffff_0000_0000) >> 32) as u32;
+    let res_lo =  (res & 0x0000_0000_ffff_ffff) as u32;
     cpu.reg[op.rdhi()] = res_hi;
     cpu.reg[op.rdlo()] = res_lo;
     if op.s() {
@@ -23,7 +23,7 @@ pub fn umull(cpu: &mut Cpu, op: SignedMlBits) -> DispatchRes {
 pub fn mul(cpu: &mut Cpu, op: MulBits) -> DispatchRes {
     let rm_val = cpu.reg[op.rm()] as usize;
     let rn_val = cpu.reg[op.rn()] as usize;
-    let res = ((rm_val * rn_val) & 0x00000000_ffffffff) as u32;
+    let res = ((rm_val * rn_val) & 0x0000_0000_ffff_ffff) as u32;
     cpu.reg[op.rd()] = res;
     if op.s() {
         cpu.reg.cpsr.set_n((res & 0x8000_0000) != 0);
@@ -38,8 +38,8 @@ pub fn umlal(cpu: &mut Cpu, op: SignedMlBits) -> DispatchRes {
     let existing: u64 = ((cpu.reg[op.rdhi()] as u64) << 32) | cpu.reg[op.rdlo()] as u64;
 
     let res = (rm_val*rn_val)+existing;
-    let res_hi = ((res & 0xffffffff_00000000) >> 32) as u32;
-    let res_lo =  (res & 0x00000000_ffffffff) as u32;
+    let res_hi = ((res & 0xffff_ffff_0000_0000) >> 32) as u32;
+    let res_lo =  (res & 0x0000_0000_ffff_ffff) as u32;
 
     cpu.reg[op.rdhi()] = res_hi;
     cpu.reg[op.rdlo()] = res_lo;
