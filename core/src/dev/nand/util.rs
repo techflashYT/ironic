13454d46ded5b88715f995da91fb6d72540c5882
Andrew Piroli <drewpiroli@gmail.com>
2023-01-10 10:50:12 -0500

    Core: NAND: Fix supressed warnings in ECC calc helper
    Setting the initial value of x was generating a warning because it was
    never read before being re-assigned. Moving the binding lower fixes this
    problem.

--- core/src/dev/nand/util.rs
+++ core/src/dev/nand/util.rs
@@ -3,20 +3,18 @@ pub fn parity(input: u8) -> u8 {
     (input.count_ones() % 2) as u8 
 }
 
-#[allow(unused_assignments)]
 pub fn calc_ecc(data: &mut [u8]) -> u32 {
     let mut a = [[0u8; 2]; 12];
     let mut a0 = 0u32;
     let mut a1 = 0u32;
-    let mut x = 0u8;
 
-    for (i, x) in data.iter().enumerate().take(512) {
+    for (idx, data) in data.iter().enumerate().take(512) {
         for j in 0..9 {
-            a[3 + j][(i >> j) & 1] ^= x;
+            a[3 + j][(idx >> j) & 1] ^= data;
         }
     }
 
-    x = a[3][0] ^ a[3][1];
+    let x: u8 = a[3][0] ^ a[3][1];
     a[0][0] = x & 0x55;
     a[0][1] = x & 0xaa;
     a[1][0] = x & 0x33;
@@ -24,11 +22,11 @@ pub fn calc_ecc(data: &mut [u8]) -> u32 {
     a[2][0] = x & 0x0f;
     a[2][1] = x & 0xf0;
 
-    for (j, aj) in a.iter_mut().enumerate() {
+    for (idx, aj) in a.iter_mut().enumerate() {
         aj[0] = parity(aj[0]);
         aj[1] = parity(aj[1]);
-        a0 |= (aj[0] as u32) << j;
-        a1 |= (aj[1] as u32) << j;
+        a0 |= (aj[0] as u32) << idx;
+        a1 |= (aj[1] as u32) << idx;
     }
 
 
