032e7cd1bc4e343b13d5fcb82a3df0305d20107c
techflashYT <officialTechflashYT@gmail.com>
2025-08-08 22:05:22 -0700

    mmio,hlwd: add multi-width device support, stub VI

--- core/src/dev/hlwd.rs
+++ core/src/dev/hlwd.rs
@@ -51,6 +51,60 @@ impl MmioDevice for AudioInterface {
     }
 }
 
+/// Legacy Video interface
+#[derive(Default, Debug, Clone)]
+pub struct VideoInterface {
+    pub clk: u16
+}
+impl MmioDeviceMultiWidth for VideoInterface {
+    fn read8(&self, off: usize) -> anyhow::Result<BusPacket> {
+        bail!("VI unsupported 8-bit read from offset {off:x}");
+    }
+
+    fn read16(&self, off: usize) -> anyhow::Result<BusPacket> {
+        let val = match off {
+            0x6c => self.clk,
+            _ => { bail!("VI 16-bit read from undefined offset {off:x}"); },
+        };
+        Ok(BusPacket::Half(val))
+    }
+    fn read32(&self, off: usize) -> anyhow::Result<BusPacket> {
+        let val = match off {
+            _ => { bail!("VI 32-bit read from undefined offset {off:x}"); },
+        };
+        Ok(BusPacket::Word(val))
+    }
+
+
+    fn write8(&mut self, off: usize, val: u8) -> anyhow::Result<Option<BusTask>> {
+        bail!("VI unsupported 8-bit write {val:02x} to offset {off:x}");
+    }
+
+    fn write16(&mut self, off: usize, val: u16) -> anyhow::Result<Option<BusTask>> {
+        match off {
+            0x6c => {
+                if val == 1 {
+                    info!(target: "VI", "Setting video clock to 54MHz");
+                } else if val == 0 {
+                    info!(target: "VI", "Setting video clock to 27MHz");
+                } else {
+                    bail!("Trying to set bogus VI clock speed {val:x}");
+                }
+                self.clk = val as u16;
+            }
+            _ => { bail!("VI 16-bit write {val:04x} to undefined offset {off:x}"); },
+        }
+        Ok(None)
+    }
+
+    fn write32(&mut self, off: usize, val: u32) -> anyhow::Result<Option<BusTask>> {
+        match off {
+            _ => { bail!("VI 32-bit write {val:08x} to undefined offset {off:x}"); },
+        }
+        Ok(None)
+    }
+}
+
 /// Legacy Processor interface
 #[derive(Default, Debug, Clone)]
 pub struct ProcessorInterface {
@@ -380,6 +434,7 @@ impl MmioDevice for AhbInterface {
 pub struct Hollywood {
     pub task: Option<HlwdTask>,
 
+    pub vi: VideoInterface,
     pub ai: AudioInterface,
     pub pi: ProcessorInterface,
     pub dsp: DSP,
@@ -417,6 +472,7 @@ impl Hollywood {
         // TODO: Where do the initial values for these registers matter?
         Ok(Hollywood {
             task: None,
+            vi: VideoInterface::default(),
             ai: AudioInterface::default(),
             pi: ProcessorInterface::default(),
             dsp: DSP::default(),
