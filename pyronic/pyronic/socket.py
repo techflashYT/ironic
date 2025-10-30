43a17ae690a16402a6b845068dcb77bc4dd08c48
techflashYT <officialTechflashYT@gmail.com>
2025-10-29 18:31:20 -0700

    Pyronic: add support for the new raw PPC I/O cmds

--- pyronic/pyronic/socket.py
+++ pyronic/pyronic/socket.py
@@ -1,15 +1,22 @@
 import socket
+from hexdump import hexdump
 from struct import pack, unpack
 WRITE_LIMIT = 10000 - 12 # back/src/ppc.rs const BUF_LEN=100000 subtract message header 12 bytes
 
 class IronicSocket(object):
     """ Representing some connection to the PPC HLE server. """
-    IRONIC_READ    = 1
-    IRONIC_WRITE   = 2
-    IRONIC_MSG     = 3
-    IRONIC_ACK     = 4
-    IRONIC_MSGNORET= 5
-    IRONIC_QUIT    = 255
+    IRONIC_READ        = 1
+    IRONIC_WRITE       = 2
+    IRONIC_MSG         = 3
+    IRONIC_ACK         = 4
+    IRONIC_MSGNORET    = 5
+    IRONIC_PPC_READ8   = 6
+    IRONIC_PPC_READ16  = 7
+    IRONIC_PPC_READ32  = 8
+    IRONIC_PPC_WRITE8  = 9
+    IRONIC_PPC_WRITE16 = 10
+    IRONIC_PPC_WRITE32 = 11
+    IRONIC_QUIT        = 255
 
     def __init__(self, filename="/tmp/ironic-ppc.sock"):
         self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -30,6 +37,58 @@ class IronicSocket(object):
         resp = self.socket.recv(size)
         assert len(resp) == size
         return resp
+
+    def send_ppc_read8(self, paddr):
+        """ Send a ppc_read8 command to the server """
+        msg = bytearray()
+        msg += pack("<LLL", self.IRONIC_PPC_READ8, paddr, 0)
+        self.socket.send(msg)
+        resp = self.socket.recv(1)
+        assert len(resp) == 1
+        return resp
+
+    def send_ppc_read16(self, paddr):
+        """ Send a ppc_read16 command to the server """
+        msg = bytearray()
+        msg += pack("<LLL", self.IRONIC_PPC_READ16, paddr, 0)
+        self.socket.send(msg)
+        resp = self.socket.recv(2)
+        assert len(resp) == 2
+        return resp
+
+    def send_ppc_read32(self, paddr):
+        """ Send a ppc_read32 command to the server """
+        msg = bytearray()
+        msg += pack("<LLL", self.IRONIC_PPC_READ32, paddr, 0)
+        self.socket.send(msg)
+        resp = self.socket.recv(4)
+        assert len(resp) == 4
+        return resp
+
+    def send_ppc_write8(self, paddr, data):
+        """ Send a ppc_write8 command to the server """
+        msg = bytearray()
+        msg += pack("<LLLB", self.IRONIC_PPC_WRITE8, paddr, 0, data)
+        self.socket.send(msg)
+        resp = self.socket.recv(2)
+        assert resp.decode('utf-8') == "OK"
+
+    def send_ppc_write16(self, paddr, data):
+        """ Send a ppc_write16 command to the server """
+        msg = bytearray()
+        msg += pack("<LLLH", self.IRONIC_PPC_WRITE16, paddr, 0, data)
+        self.socket.send(msg)
+        resp = self.socket.recv(2)
+        assert resp.decode('utf-8') == "OK"
+
+    def send_ppc_write32(self, paddr, data):
+        """ Send a ppc_write32 command to the server """
+        msg = bytearray()
+        msg += pack("<LLLL", self.IRONIC_PPC_WRITE32, paddr, 0, data)
+        self.socket.send(msg)
+        resp = self.socket.recv(2)
+        assert resp.decode('utf-8') == "OK"
+
 
     def handle_large_guestwrite(self, paddr, buf):
         offset = 0
