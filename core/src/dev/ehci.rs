7d3b738e927f0c3421d84c8ef9810a3e678b64db
techflashYT <officialTechflashYT@gmail.com>
2026-08-15 18:46:12 -0500

    hlwd: ehci: stub a bunch more registers

--- core/src/dev/ehci.rs
+++ core/src/dev/ehci.rs
@@ -5,10 +5,12 @@ use crate::bus::prim::*;
 use crate::bus::mmio::*;
 use crate::bus::task::*;
 
-/// Representing the SHA interface.
+/// Representing the Enhanced Host Controller Interface.
 #[derive(Default)]
 pub struct EhcInterface {
-    pub unk_14: u32,
+    pub usbcmd: u32,
+    pub usbsts: u32,
+    pub usbintr: u32,
     pub unk_a4: u32,
     pub unk_b0: u32,
     pub unk_b4: u32,
@@ -17,7 +19,9 @@ pub struct EhcInterface {
 impl EhcInterface {
     pub fn new() -> Self {
         EhcInterface {
-            unk_14: 0,
+            usbcmd: 0x0000_0001, /* RS=1 */
+            usbsts: 0,
+            usbintr: 0,
             unk_a4: 0,
             unk_b0: 0,
             unk_b4: 0,
@@ -31,7 +35,15 @@ impl MmioDevice for EhcInterface {
 
     fn read(&self, off: usize) -> anyhow::Result<BusPacket> {
         let val = match off {
-            0x14 => self.unk_14,
+            /*
+             * The real intended value is 0x10, but it appears like this due
+             * to the hardware byteswapper.  Software masks out everything
+             * but the lowest 8 bits.
+             */
+            0x00 => 0x0100_0010,
+            0x10 => self.usbcmd,
+            0x14 => self.usbsts,
+            0x18 => self.usbintr,
             0xa4 => self.unk_a4,
             0xb0 => self.unk_b0,
             0xb4 => self.unk_b4,
@@ -43,7 +55,19 @@ impl MmioDevice for EhcInterface {
 
     fn write(&mut self, off: usize, val: u32) -> anyhow::Result<Option<BusTask>> {
         match off {
-            0x14 => self.unk_14 = val,
+            0x10 => {
+                if (self.usbcmd & 1 == 0) && (val & 1 == 1) {
+                    // setting RS=1, set HCHALTED=0
+                    self.usbsts &= !0x0000_1000;
+                }
+                else if (self.usbcmd & 1 == 1) && (val & 1 == 0) {
+                    // setting RS=0, set HCHALTED=1
+                    self.usbsts |= 0x0000_1000;
+                }
+                self.usbcmd = val;
+            },
+            0x14 => self.usbsts = val,
+            0x18 => self.usbintr = val,
             0xa4 => self.unk_a4 = val,
             0xb0 => self.unk_b0 = val,
             0xb4 => self.unk_b4 = val,
